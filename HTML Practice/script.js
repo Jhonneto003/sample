5d5d02630dd983539320d0bea28ce93ea33f51f9
Jhonneto003 <109934489+Jhonneto003@users.noreply.github.com>
2024-01-28 09:42:19 -0800

    Add files via upload

---
+++ HTML Practice/script.js
@@ -0,0 +1,15 @@
+alert("Hi I'm an alert box.");
+function changeContent() {
+   	    document.getElementById("sample").innerHTML = "Lyon Mining";
+       }
+function show() {
+document.getElementById("sample").style.display = "block"
+     }
+ 
+  function hide() {
+      document.getElementById("sample"). style.display = "none"
+  }
+  
+  function changeImage() {
+      document.getElementById("sample").src= "myself.jpg";
+  }
